efd03176a37a0a4ecfb0cd84c2f1a65584f7add1
qwelyt <qwelyt@users.noreply.github.com>
2014-03-18 12:43:03 +0100

    Changed AP textarea to a JList.

--- CSWIDS.java
+++ CSWIDS.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class CSWIDS extends JFrame{
 	private JMenuBar menuBar;
@@ -10,7 +11,11 @@ public class CSWIDS extends JFrame{
 	private JPanel buttonBar, main;
 	private JButton bTest;
 
-	private JTextArea tmpTxt, log;
+	private JTextArea log;
+	private JList apList;
+	//private ArrayList<String> apData;
+	private DefaultListModel apData;
+
 
 	public void init(){
 		setTitle("CSWIDS");
@@ -20,11 +25,26 @@ public class CSWIDS extends JFrame{
 		main = new JPanel();
 		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
 
+		/*
 		tmpTxt = new JTextArea(10,50);
 		tmpTxt.setLineWrap(true);
 		tmpTxt.setEditable(false);
 		JScrollPane sp = new JScrollPane(tmpTxt);
 		tmpTxt.append("Here is where the AP should be shown");
+		*/
+
+		//apData = new ArrayList<String>("ap1","ap2","ap3");
+		apData = new DefaultListModel();
+		for(int i=0; i< 10; i++)
+			apData.addElement("ap"+i);
+		apList = new JList(apData);
+		apList.setSelectionModel(new setSelectionModel(apList,2));
+		apList.setLayoutOrientation(JList.VERTICAL);
+		apList.setVisibleRowCount(10);
+		JScrollPane spAP = new JScrollPane(apList);
+		spAP.setPreferredSize(new Dimension(20, 150));
+
+
 
 		log = new JTextArea(10,50);
 		log.setLineWrap(true);
@@ -32,7 +52,7 @@ public class CSWIDS extends JFrame{
 		JScrollPane spLog = new JScrollPane(log);
 		log.append("This is where the log information will be outputed.");
 
-		main.add(sp);
+		main.add(spAP);
 		main.add(spLog);
 		add(main);
 
@@ -84,5 +104,37 @@ public class CSWIDS extends JFrame{
 
 	public static void main(String[] args){
 		new CSWIDS().init();
+	}
+
+	private static class setSelectionModel extends DefaultListSelectionModel{
+		private JList list;
+		private int maxCount;
+
+		private setSelectionModel(JList list,int maxCount){
+			this.list = list;
+			this.maxCount = maxCount;
+		}
+
+		@Override
+		public void setSelectionInterval(int index0, int index1){
+			if (index1 - index0 >= maxCount){
+			index1 = index0 + maxCount - 1;
+			}
+			super.setSelectionInterval(index0, index1);
+		}
+
+		@Override
+		public void addSelectionInterval(int index0, int index1){
+			int selectionLength = list.getSelectedIndices().length;
+			if (selectionLength >= maxCount)
+				return;
+		
+			if (index1 - index0 >= maxCount - selectionLength){
+				index1 = index0 + maxCount - 1 - selectionLength;
+			}
+			if (index1 < index0)
+				return;
+			super.addSelectionInterval(index0, index1);
+		}
 	}
 }
